80a9c20d085187be639899b683267db3c6bcf5ad
Vaibhav Gupta <81480233+vibhu2408@users.noreply.github.com>
2021-10-07 22:15:10 +0530

    Update task_1.cpp

--- College/task_1.cpp
+++ College/task_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
  {
@@ -6,15 +7,14 @@ int main()
     cout << "Enter a string" << endl;
     getline(cin, s);
     
-    string :: iterator.it;
-    for(it = s.begin(); it = s.end(); i++) {
+    string :: iterator it;
+    for(it = s.begin(); it != s.end(); it++) {
     rev = *it + rev;
     }
     
-    cout << "Reverse of string" << rev << endl;
+    cout << "Reverse of string(manually): " << rev << endl;
     
-    temp = reverse(s.begin(), s.end());
-    cout << "Reverse of string" << temp << endl;
+    reverse(s.begin(), s.end());
+    cout << "Reverse of string(using built in): " << s << endl;
     return 0;
 }
-    
